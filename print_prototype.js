57e4c7d44283fa3d4ff4d23544381d67306b80d2
kyomin <kim031504@naver.com>
2022-03-12 22:41:06 +0900

    객체의 프로토타입 출력

---
+++ print_prototype.js
@@ -0,0 +1,14 @@
+// 객체의 프로토타입 출력
+
+// [1]: __proto__ vs Object.getPrototypeOf()
+function A() {}
+let obj = new A();
+
+console.log(obj.__proto__); // A function
+console.log(Object.getPrototypeOf(obj)); // A function
+
+// [2]: ===
+function B() {}
+let obj2 = new B();
+
+console.log(obj2.__proto__ === Object.getPrototypeOf(obj2)); // true

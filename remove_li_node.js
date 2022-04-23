1c1e1222c576a194d569e2762fec69d2fa225044
kyomin <kim031504@naver.com>
2022-04-23 12:49:36 +0900

    NodeList 객체와 Array 객체의 차이점

--- remove_li_node.js
+++ remove_li_node.js
@@ -3,6 +3,7 @@
 // [1]: li의 문자열 값으로 해당 요소 삭제 --> 함수 만들기
 function removeElementByText(text) {
 	const li = document.querySelectorAll('li'); // NodeList 객체를 반환
+	console.log(li);
 
 	// 1. 자바스크립트에서 Element 배열을 다룰 시 --> querySelectorAll은 NodeList 객체를 반환(배열과 차이점이 있다)
 	// 2. NodeList 객체의 프로토타입 메서드 --> forEach 메서드를 이용하여 순회하면서 각 엘리먼트를 비교하여 조건 처리
@@ -10,11 +11,31 @@ function removeElementByText(text) {
 	// 4. 그러면 이 메서드는 브라우저의 API라는 것을 알 수 있다. 그런데 JS에서도 호출해서 사용할 수 있다.
 	// 5. NodeList와 Array의 가장 큰 차이점 --> NodeList는 자바스크립트 API가 아니고, 브라우저가 제공하는 API
 
+	const li2 = [...document.querySelectorAll('li')]; // Array 객체를 반환
+	console.log(li2);
+
 	li.forEach((element) => {
 		if (element.innerText === text) {
 			element.parentNode.removeChild(element);
 		}
 	});
+
+	// NodeList vs Array
+	// 1. 배열의 길이에 대한 정보를 가진다. --> length 속성. 이 부분은 두 객체 모두 공통점
+	console.log(li.length); // 4
+	console.log(li2.length); // 4
+	console.log('----------------------------------------');
+
+	// 2. NodeList의 item() 메서드
+	console.log(li.item(0)); // 첫 번째 li 요소
+	console.log(li.item(0).innerText); // 홍길동
+	console.log(li2[1]); // 두 번째 li 요소
+	console.log(li2[1].innerText); // 이순신
+	console.log('----------------------------------------');
+
+	// 3. 참조를 벗어날 때
+	console.log(li.item(4)); // null
+	console.log(li2[4]); // undefined
 }
 
 // [2]: 함수 호출
